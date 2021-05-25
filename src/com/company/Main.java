6e97bc54f94ca2e481beee052573cb9792b351f9
Dmitriy Mikhailoff <dmitriy.mikhailoff@gmail.com>
2021-05-25 20:56:21 +0300

    added new function 'add'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("First change");
+        System.out.println(add(3,3));
+    }
+
+    public static int add (int a, int b) {
+        return a + b;
     }
 }
